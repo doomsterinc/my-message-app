52167cbda4a7c419db8d2aaaf28ac5e7345b14aa
doomsterinc <doomsterinc1@gmail.com>
2016-12-20 03:43:04 -0200

    ADD first post request

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.post('/api/message', function(req, res){
+    console.log(req.body);
+    res.status(200);
+});
+
+var server = app.listen(5000, function (){
+    console.log("listening in port " + server.address().port);
+});
