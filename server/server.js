e6c22e1c9b1aa65f8621c824ed4a4c3cef2147e7
doomsterinc <doomsterinc1@gmail.com>
2016-12-20 05:37:21 -0200

    Add MongoDB connector

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongo = require('mongodb').MongoClient;
 
 app.use(bodyParser.json());
+
+var database;
 
 app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
@@ -12,8 +15,17 @@ app.use(function(req, res, next){
 
 app.post('/api/message', function(req, res){
     console.log(req.body);
+    database.collection('messages').insertOne(req.body);
+
     res.send(req.body);
     res.status(200);
+});
+
+mongo.connect('mongodb://localhost:27017/test', function(err, db){
+    if(!err){
+        console.log('we are connected to mongo');
+    }
+    database = db;
 });
 
 var server = app.listen(5000, function (){
