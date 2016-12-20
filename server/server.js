87d103673a0cbc474817316169f9ae20a50203e1
doomsterinc <doomsterinc1@gmail.com>
2016-12-20 06:14:54 -0200

    Modified app to use mongoose

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 
-var database;
+var Message = mongoose.model('Message', {
+    msg: String
+});
 
 app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
@@ -15,17 +17,25 @@ app.use(function(req, res, next){
 
 app.post('/api/message', function(req, res){
     console.log(req.body);
-    database.collection('messages').insertOne(req.body);
+    var message = new Message(req.body);
+
+    message.save();
 
     res.send(req.body);
     res.status(200);
 });
 
-mongo.connect('mongodb://localhost:27017/test', function(err, db){
+function GetMessages(){
+  Message.find({}).exec(function(err, result){
+      console.log(result);
+  });
+};
+
+mongoose.connect('mongodb://localhost:27017/test', function(err, db){
     if(!err){
         console.log('we are connected to mongo');
     }
-    database = db;
+    GetMessages();
 });
 
 var server = app.listen(5000, function (){
